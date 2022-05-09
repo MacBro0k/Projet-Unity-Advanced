c76b20b41d3bbcd078ef5d5a9be7319be983d028
MacBro0k <104989153+MacBro0k@users.noreply.github.com>
2022-05-09 17:53:28 +0200

    GameManager (Incomplet)
    Ajout d'un menu + script GameManager qui permet pour le moment seulement de charger des niveaux.
    
    Update Boot.unity

--- Game project 1/Assets/Script/GameManager.cs
+++ Game project 1/Assets/Script/GameManager.cs
@@ -5,12 +5,63 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public void RestartGame(string SceneName){
-        Debug.Log("restart");
-        SceneManager.LoadScene(SceneName);
+    // Fonctionnalités à mettre en place :
+    // - Garder en mémoire le niveau du jeu courant
+    // - Charger et décharger les différents niveaux du jeu
+    // - Garder une trace de l'état courant du jeu
+    // - Générer d'autres systèmes persistants
+
+    private string _currentLevelName = string.Empty;
+    List<AsyncOperation> _loadOperations;
+    private static GameManager instance;
+
+    private void Awake() {
+        if(instance == null) {
+            instance = this;
+        } 
+        else {
+            Destroy(gameObject);
+            Debug.LogError("Yo don't do this bruh");
+ }
+    }
+    private void Start() {
+        LoadLevel("SampleScene");
+        _loadOperations = new List<AsyncOperation>();
+        DontDestroyOnLoad(this.gameObject);
+        
+    }
+    public void LoadLevel(string levelName) {
+        AsyncOperation ao = SceneManager.LoadSceneAsync(levelName, LoadSceneMode.Additive);
+        ao.completed += OnLoadOperationComplete;
+        _loadOperations.Add(ao);
+
+        if (ao != null) {
+            Debug.LogError("[GameManager] Unable to load level" + levelName);
+            return;
+        }
+        
+        _currentLevelName = levelName;
+    }
+    public void UnloadLevel(string levelName) {
+        AsyncOperation ao = SceneManager.UnloadSceneAsync(levelName);
+        ao.completed += OnUnloadOperationComplete;
+
+        if (ao != null) {
+            Debug.LogError("[GameManager] Unable to unload level" + levelName);
+            return;
+        }
+        _currentLevelName = levelName;
     }
 
-    public void Quitgame(){
-        Application.Quit();
+    void OnLoadOperationComplete(AsyncOperation elem) {
+        if (_loadOperations.Contains(elem)) {
+            _loadOperations.Remove(elem);
+        }
+        UnloadLevel(_currentLevelName);
+        Debug.Log("Load Complete");
+    }
+
+    void OnUnloadOperationComplete(AsyncOperation elem) {
+        Debug.Log("Unload Complete");
     }
 }
---
+++ Game project 1/Assets/Script/UIManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public void RestartGame(string SceneName){
+        Debug.Log("restart");
+        SceneManager.LoadScene(SceneName);
+    }
+
+    public void Quitgame(){
+        Application.Quit();
+    }
+}
