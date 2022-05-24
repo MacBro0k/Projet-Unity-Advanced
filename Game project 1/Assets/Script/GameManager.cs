6e05727ba26349b8a94888d3d23f1d2e9ba506aa
MacBro0k <104989153+MacBro0k@users.noreply.github.com>
2022-05-24 12:19:10 +0200

    Modification du GameManager
    Modification de GameManager.cs + ajout de Singleton.cs

--- Game project 1/Assets/Script/GameManager.cs
+++ Game project 1/Assets/Script/GameManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
+public class GameManager : Singleton<GameManager>
 {
     // Fonctionnalités à mettre en place :
     // - Garder en mémoire le niveau du jeu courant
---
+++ Game project 1/Assets/Script/Utils/Singleton.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+    private static GameManager instance;
+    private static bool isInitialized;
+
+    public static T Instance {
+        get { return instance; }
+    }
+
+    protected virtual void Awake() {
+        if(instance != null) {
+            Debug.LogError("[Singleton] Trying to instantiate a second instance of a singleton class.");
+        } 
+        else
+            instance = (T)this;
+    }
+
+    protected virtual void OnDestroy() {
+        if (instance == this)
+            instance = null;    
+    }
+
+    public static bool IsInitialized {
+        get { return instance; }
+    }
+}
