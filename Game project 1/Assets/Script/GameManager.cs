59f4fc8ba78b11ffd596479b5501f344b1d35777
MacBro0k <104989153+MacBro0k@users.noreply.github.com>
2022-06-07 19:31:46 +0200

    C bon jen ai marre

--- Game project 1/Assets/Script/GameManager.cs
+++ Game project 1/Assets/Script/GameManager.cs
@@ -13,12 +13,16 @@ public class GameManager : Singleton<GameManager>
 
     private string _currentLevelName = string.Empty;
     List<AsyncOperation> _loadOperations;
+    public GameObject[] SystemPrefabs;
+    private List<GameObject> _instanciedSystemPrefabs;
+
     
     private void Start() {
-        LoadLevel("SampleScene");
+        LoadLevel("Lvl_1-2");
         _loadOperations = new List<AsyncOperation>();
         DontDestroyOnLoad(this.gameObject);
-        
+        _instanciedSystemPrefabs = new List<GameObject>();
+        InstantiateSystemPrefabs();
     }
     public void LoadLevel(string levelName) {
         AsyncOperation ao = SceneManager.LoadSceneAsync(levelName, LoadSceneMode.Additive);
@@ -53,5 +57,25 @@ public class GameManager : Singleton<GameManager>
 
     void OnUnloadOperationComplete(AsyncOperation elem) {
         Debug.Log("Unload Complete");
+    }
+
+    void InstantiateSystemPrefabs() {
+        GameObject prefabInstance;
+        for (int i = 0; i < SystemPrefabs.Length; i++) {
+            prefabInstance = Instantiate(SystemPrefabs[i]);
+            _instanciedSystemPrefabs.Add(prefabInstance);
+
+        }
+    }
+
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
+
+        for (int i = 0; i < _instanciedSystemPrefabs.Count; i++) {
+            Destroy(_instanciedSystemPrefabs[i]);
+        }
+
+        _instanciedSystemPrefabs.Clear();
     }
 }
