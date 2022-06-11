575a39bacca07d5b0e6732cc0152cb760163b8a1
yannleboss <Yann.riani@gmail.com>
2022-06-11 16:33:13 +0200

    Bug fix

--- Game project 1/Assets/Script/PlayerScript/PlayerShooter.cs
+++ Game project 1/Assets/Script/PlayerScript/PlayerShooter.cs
@@ -10,6 +10,9 @@ public class PlayerShooter : MonoBehaviour
     public GameObject Bullet;
     public GameObject BlastEffect;
     
+    public float ColdownTime = 1f;
+
+    private float endColdownTime;
     private bool m_canShoot = true;
     private Animator m_animator;
 
@@ -25,30 +28,26 @@ public class PlayerShooter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Disable Shoot if Character Speaking
-        if(DialogueManager.GetInstance().dialogueIsPlaying){
-            m_canShoot = false;
-        }
-        else if (!m_canShoot)
-        {
-            m_canShoot = true;
-        }
+        m_canShoot = transform.root.gameObject.GetComponent<PrototypeHeroControler>().m_canShoot;
 
-        if(m_canShoot){
+        if(m_canShoot && Time.time >= endColdownTime){
             if (Input.GetButtonDown("Fire1")){
-                if(m_animator)
-                    m_animator.SetTrigger("ShootingB");
-                Shoot();
+                if(m_animator){
+                    m_animator.SetBool("IsCharging", true);
+                }else{
+                    Shoot();
+                }
             }
         }
     }
 
     void Shoot (){
+        endColdownTime = Time.time + ColdownTime;
         Instantiate(Bullet, firepoint.position, firepoint.rotation);
         Instantiate(BlastEffect, firepoint.position, firepoint.rotation).transform.SetParent(gameObject.transform);
+        if(m_animator){
+            m_animator.SetBool("IsCharging", false);
+        }
     }
 
-    void CantShoot(){
-        m_canShoot = !m_canShoot;
-    }
 }
