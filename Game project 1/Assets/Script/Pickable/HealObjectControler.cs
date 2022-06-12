fedf18728ced1b07d106dd7f6e3269202fcb9d09
yannleboss <Yann.riani@gmail.com>
2022-06-12 18:42:40 +0200

    fix des bug de william pck ce con il a fait de la merde sur le perso

--- Game project 1/Assets/Script/Pickable/HealObjectControler.cs
+++ Game project 1/Assets/Script/Pickable/HealObjectControler.cs
@@ -8,10 +8,9 @@ public class HealObjectControler : MonoBehaviour
     public int Amount;
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Player" && collision.GetComponent<PlayerInventory>().Life < collision.GetComponent<PlayerInventory>().MaxLife) { // Nous recuperons le tag de l'objet en colision 
-            Destroy(gameObject.transform.root.gameObject); // On detruit la hierarchie complete de l'objet rubis 
+        if(collision.tag == "Player" && collision.GetComponent<PlayerInventory>().Life < collision.GetComponent<PlayerInventory>().MaxLife) { // Check if pleyer is not full life
+            Destroy(gameObject.transform.root.gameObject);
             collision.GetComponent<PlayerInventory>().Heal(Amount);
-            //Instantiate(HealSound, transform.position, Quaternion.identity); // transform.position nous permet de recuperer la position de l'objet actuel 
         } 
     }
 }
