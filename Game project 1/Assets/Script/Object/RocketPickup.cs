fa2a65e77975ae3e9eee30e2f0110a02b4f0c3f1
MacBro0k <104989153+MacBro0k@users.noreply.github.com>
2022-06-13 01:03:20 +0200

    Prefab Rocket Munition
    fix munition

--- Game project 1/Assets/Script/Object/RocketPickup.cs
+++ Game project 1/Assets/Script/Object/RocketPickup.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AmmoPickup : MonoBehaviour
+public class RocketPickup : MonoBehaviour
 {
     public GameObject PickupSound;
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.CompareTag("Player")) {
-            // other.GetComponent<PlayerInventory>().AddRocket(1);
-            other.GetComponent<PlayerInventory>().ChargeLaser(100);
+            other.GetComponent<PlayerInventory>().AddRocket(1);
             Instantiate(PickupSound);
+            Destroy(gameObject.transform.root.gameObject);
         }
     }
 }
